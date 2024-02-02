74622fa7cffbea068114cb1078f5794704e6daf4
Promise Nwafor <promisenwafor955@gmail.com>
2024-02-02 22:37:04 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Modal App</h1>
+  <teleport to="#modals" v-if="showModal">
+    <Modal
+      :header="header"
+      toggleModal="toggleModal"
+      color="pitch"
+      @close="toggleModal"
+    >
+      <h2>Top modal</h2>
+      <template v-slot:emy>
+        <h3>Emy</h3>
+      </template>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">Open Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      showModal: false,
+      header: "Mark",
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
